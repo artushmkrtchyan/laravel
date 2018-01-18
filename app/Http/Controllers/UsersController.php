093055898c98ccbb99374d4e7b549976343ca05e
Artush <artyusha01@mail.ru>
2018-01-18 16:17:10 +0400

    ...

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,17 +17,6 @@ class UsersController extends Controller
      */
     public function index()
     {
-       /* $data = [
-			'users' => array(
-				array('name' => "user 1", "description"  => "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", "avatar" => "https://www.w3schools.com/bootstrap/img_avatar1.png"),
-				array('name' => "user 2", "description"  => "Lorem Ipsum has been the industry's standard dummy text  to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", "avatar" => "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png"),
-				array('name' => "user 3", "description"  => "when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", "avatar" => "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/CandymyloveYasu.png/220px-CandymyloveYasu.png"),
-				array('name' => "user 4", "description"  => "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ", "avatar" => "https://www.w3schools.com/bootstrap/img_avatar1.png")
-			)
-		];
-		
-        return view('users.users', $data);*/
-		
     	return view('users.users', array('users' => User::all()) );
     }
 
